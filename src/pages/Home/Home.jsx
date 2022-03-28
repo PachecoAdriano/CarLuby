d30f9212698fd3f8c29c50f31ec14a26bf148c72
Adriano <adrianopacheco@gmail.com>
2022-03-27 22:12:27 -0300

    primeiro commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,100 +1,71 @@
-import {useState} from 'react'
-import logo from '../../assets/imgs/logo.png'
-import exit from '../../assets/icons/exit.png'
-import Foto1 from '../../assets/imgs/Card_Img1.png'
-import Foto2 from '../../assets/imgs/Card-img2.png'
-import Foto3 from '../../assets/imgs/Card-img3.png'
-import { useNavigate } from 'react-router-dom'
-import './Home.css'
-
-
+import { useState } from "react";
+import logo from "../../assets/imgs/logo.png";
+import exit from "../../assets/icons/exit.png";
+import search from "../../assets/icons/search.png";
+import Foto1 from "../../assets/imgs/Card_Img1.png";
+import Foto2 from "../../assets/imgs/Card-img2.png";
+import Foto3 from "../../assets/imgs/Card-img3.png";
+import { useNavigate } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
 
-   
-
-   const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <>
-       <nav>
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="logo"></img>
+          <p className="title-logo">AutoLuby</p>
+          <input type="search" className="search" >            
+            </input>
+        </div>
 
-    
-<div className='logo'>
- <img src={logo} alt='logo'></img>
- <p className='title-logo'>AutoLuby</p>
- </div>
+        <button className="btn-logout">
+          Sair
+          <img src={exit} alt="exit"></img>
+        </button>
+        <section>
+          <p className="p1">Bem-vindo, Adriano</p>
+          <label className="menu">Menu</label>
+        </section>
 
-
- 
-
-  <button  className="btn-logout">Sair
-  <img src={exit} alt='exit'></img></button>
-
-  <div className='container-card'>
-
-  <section className='Card1'>
-  <div className='container-card-view1'>
-  <section className='card-view1'>
-  <section className='card-title'>
-  
-  <h1>Veículos reservados e vendidos </h1>
-     <h2 className='subtitle'>Veículos reservados e vendidos por você</h2>
-     </section>
-     <h3>180 Veículos</h3>
-     <img src={Foto1} alt='Foto1'></img>
-     
-  </section>
-       
-  </div>
-  </section>
-
-  <section className='Card2'>
-  <div className='container-card-view2'>
-  <section className='card-view2'>
-  <section className='card-title'>
-  
-  <h1>Listagem geral de veículos </h1>
-     <h2 className='subtitle'>Listagem de veículos de toda a empresa</h2>
-     </section>
-     <h3>180 Veículos</h3>
-     <img onClick={() => navigate('/cars')} src={Foto2} alt='Foto2'></img>
-     
-  </section>
-       
-  </div>
-  </section>
-
-  <section className='Card3'>
-  <div className='container-card-view3'>
-  <section className='card-view3'> 
-  <section className='card-title'>
-  
-  <h1>Funcionários da empresa </h1>
-  
-  
-     <h2 className='subtitle'>Listagem de todos os funcionários da empresa</h2>
-     
-     </section>
-     <h3 >180 Veículos</h3>
-     
-     <img onClick={() => navigate('/employees')} src={Foto3} alt='Foto3'></img>
-     <section>
-     </section>
-  </section>
-  
-  </div>
-  </section>
+        <div className="div-central">
+          <div className="card" >
+            <div className="section">
+              <img src={Foto1} alt="Foto1" />
+            </div>
+            <p className="card-p1"> Veículos reservados e vendidos</p>
+            <label className="card-p2"> Veículos reservados e vendidos por você</label>
+            <label className="card-p3"> 0 veículos</label>
+          </div>
+            <section hight='10px'/>
 
 
-  
-       
-  </div>
-  
+          <div className="card" onClick={() => navigate('/cars')}>
+            <div className="section">
+              <img src={Foto2} alt="Foto2" />
+            </div>
+            <p className="card-p1"> Listagem geral de veículos</p>
+            <label className="card-p2"> Listagem de veículos de toda a empresa</label>
+            <p className="card-p3"> 7 veículos</p>
+          </div>
+          <section height='10px'/>
 
- </nav>
+
+          <div className="card" onClick={() => navigate('/employees')}>
+            <div className="section">
+              <img src={Foto3} alt="Foto3" />
+            </div>
+            <p className="card-p1"> Funcionários da empresa</p>
+            <label className="card-p2"> Listagem de todos os funcionários da empresa</label>
+            <p className="card-p3"> 5 funcionários</p>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/cars/Cars.jsx
+++ src/pages/cars/Cars.jsx
@@ -1,57 +1,56 @@
 import logo from "../../assets/imgs/logo.png";
 import "./Cars.css";
 import exit from "../../assets/icons/exit.png";
-import search from "../../assets/icons/search.png";
-
-//import carsData from '../../CarsData'
 
 function cars() {
   return (
-    <div className="logo">
-      <img src={logo} alt="logo"></img>
-      <p className="title-logo">AutoLuby</p>
-      <div>
-        <h1 className="title">Seus Veículos</h1>
-        <button className="btn-logout">
-          Sair
-          <img src={exit} alt="exit" />
-        </button>
+    <div>
+      <div className='logo'>
+        <img src={logo} alt='logo'></img>
+        <p className='title-logo'>AutoLuby</p>
       </div>
 
-      <p className="title2">Listagem de veículos reservados e vendidos</p>
-      <input type="search" className="search"></input>
+      <div>
+        <h1 className="title">Seus Veículos</h1>
+        <button className="btn-logout">Sair<img src={exit} alt='exit'></img></button>
+      </div>
 
-     
-      <ul class="pagination">
-        <li >
-          <a href="#" target="_self">
-            Anterior
-          </a>
-        </li>
-        <li>
-          <a href="#">1</a>
-        </li>
+      <div>
+        <p className="title2">Listagem de veículos reservados e vendidos</p>
+        <ul class="pagination">
+          <li >
+            <a href="#" target="_self">
+              Anterior
+            </a>
+          </li>
+          <li>
+            <a href="#">1</a>
+          </li>
 
-        <li>
-          <a class="active">2</a>
-        </li>
+          <li>
+            <a class="active">2</a>
+          </li>
 
-        <li>
-          <a href="#">3</a>
-        </li>
+          <li>
+            <a href="#">3</a>
+          </li>
 
-        <li>
-          <a href="#" target="_self">
-            Proximo
-          </a>
-        </li>
-      </ul>
-    
+          <li>
+            <a href="#" target="_self">
+              Proximo
+            </a>
+          </li>
+        </ul>
+        <input type="search" className="search">
+         
+        </input>
+      </div>
+
       <div className="central">
-        <div className="cars">
+        <div className="cars">   
           <table>
-            <thead className="t-head">
-              <tr className="tr-header">
+            <thead >
+              <tr >
                 <th>Marca</th>
                 <th>Modelo</th>
                 <th>Ano</th>
@@ -62,25 +61,14 @@ function cars() {
                 <th>Valor</th>
               </tr>
             </thead>
-            <tbody className="tr-body">
+            <tbody >
               <tr>
                 <td>Ford</td>
                 <td>Fiesta</td>
                 <td>2017</td>
                 <td>70.000</td>
                 <td>Branco</td>
-                <td className="sold">Vendido</td>
-                <td>9BW</td>
-                <td>R$ 39.000,00</td>
-              </tr>
-
-              <tr>
-                <td>Ford</td>
-                <td>Fiesta</td>
-                <td>2017</td>
-                <td>70.000</td>
-                <td>Branco</td>
-                <td className="reserved">Reservado</td>
+                <td className="sold"><span>Vendido</span></td>
                 <td>9BW</td>
                 <td>R$ 39.000,00</td>
               </tr>
@@ -90,7 +78,7 @@ function cars() {
                 <td>2017</td>
                 <td>70.000</td>
                 <td>Branco</td>
-                <td className="reserved">Reservado</td>
+                <td className="reserved"><span>Reservado</span></td>
                 <td>9BW</td>
                 <td>R$ 39.000,00</td>
               </tr>
@@ -100,7 +88,17 @@ function cars() {
                 <td>2017</td>
                 <td>70.000</td>
                 <td>Branco</td>
-                <td className="sold">Vendido</td>
+                <td className="reserved"><span>Reservado</span></td>
+                <td>9BW</td>
+                <td>R$ 39.000,00</td>
+              </tr>  
+              <tr>
+                <td>Ford</td>
+                <td>Fiesta</td>
+                <td>2017</td>
+                <td>70.000</td>
+                <td>Branco</td>
+                <td className="sold"><span>Vendido</span></td>
                 <td>9BW</td>
                 <td>R$ 39.000,00</td>
               </tr>
@@ -110,38 +108,37 @@ function cars() {
                 <td>2017</td>
                 <td>70.000</td>
                 <td>Branco</td>
-                <td className="sold">Vendido</td>
+                <td className="sold"><span>Vendido</span></td>
                 <td>9BW</td>
                 <td>R$ 39.000,00</td>
               </tr>
-
               <tr>
                 <td>Ford</td>
                 <td>Fiesta</td>
                 <td>2017</td>
                 <td>70.000</td>
                 <td>Branco</td>
-                <td className="reserved">Reservado</td>
+                <td className="reserved"><span>Reservado</span></td>
                 <td>9BW</td>
                 <td>R$ 39.000,00</td>
               </tr>
-
               <tr>
                 <td>Ford</td>
                 <td>Fiesta</td>
                 <td>2017</td>
                 <td>70.000</td>
                 <td>Branco</td>
-                <td className="reserved">Reservado</td>
+                <td className="reserved"><span>Reservado</span></td>
                 <td>9BW</td>
                 <td>R$ 39.000,00</td>
-              </tr>
+              </tr> 
             </tbody>
           </table>
         </div>
+        
       </div>
     </div>
+   
   );
-}
-
+   }
 export default cars;
